d93c57813480cd6c1340efc9f4e32702ced326f1
bd-dev-ismail <ismailratul7@gmail.com>
2022-10-25 23:32:06 +0600

    making fake data & enabel cors

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000
-
-
+app.use(cors())
 app.get('/', (req, res)=>{
     res.send('Freelancing Educare Institute Server is running Sucessfully')
 })
+
 
 app.listen(port , ()=>{
     console.log('Freelancing Educare Institute Server is runnign on port', port)
