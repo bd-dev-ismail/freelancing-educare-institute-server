81aff6cf0a11f47039fdbf972b7cf36062132a12
bd-dev-ismail <ismailratul7@gmail.com>
2022-10-26 01:36:09 +0600

    make data for all courses

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
 app.use(cors())
+const about = require('./data/about.json')
+const courses = require('./data/courses.json')
 app.get('/', (req, res)=>{
     res.send('Freelancing Educare Institute Server is running Sucessfully')
 })
-
-
+//about course
+app.get('/about', (req, res)=>{
+    res.send(about)
+})
+//courses
+app.get('/courses', (req, res)=>{
+    res.send(courses)
+})
 app.listen(port , ()=>{
     console.log('Freelancing Educare Institute Server is runnign on port', port)
 })
