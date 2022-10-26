a74577c2350d56a79ddc6936fbb6c83f62308521
bd-dev-ismail <ismailratul7@gmail.com>
2022-10-26 11:21:43 +0600

    checkout page fake data

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000
 app.use(cors())
 const about = require('./data/about.json')
 const courses = require('./data/courses.json')
-const CoursesDetails = require('./data/courseDetails.json')
+const coursesDetails = require('./data/courseDetails.json')
 app.get('/', (req, res)=>{
     res.send('Freelancing Educare Institute Server is running Sucessfully')
 })
@@ -20,8 +20,14 @@ app.get('/courses', (req, res)=>{
 //courses details
 app.get('/courses/:id', (req, res)=>{
     const id = req.params.id;
-    const details = CoursesDetails.find((details) => details.category_id == id)
+    const details = coursesDetails.find((details) => details.category_id == id)
     res.send(details)
+})
+//checkout
+app.get('/checkout/:id', (req, res)=>{
+    const id = req.params.id;
+    const checkout = coursesDetails.find(cart => cart.category_id == id)
+    res.send(checkout)
 })
 app.listen(port , ()=>{
     console.log('Freelancing Educare Institute Server is runnign on port', port)
